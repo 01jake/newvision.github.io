c5f3e851a22f5a78409cd664eb33c7837e83a9e3
01jake <ang314256@gmail.com>
2025-02-21 00:07:32 -0700

    actualizacion

--- src/app/cocina/cocina.component.ts
+++ src/app/cocina/cocina.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import {  OnInit, HostListener } from '@angular/core';
 @Component({
   selector: 'app-cocina',
   imports: [RouterModule],
@@ -10,5 +10,32 @@ import { RouterModule } from '@angular/router';
 
 })
 export class CocinaComponent {
+  constructor() { }
 
+  ngOnInit(): void {
+    this.checkVisibility();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.checkVisibility();
+  }
+
+  checkVisibility() {
+    const categories = document.querySelectorAll('.category');
+    categories.forEach(category => {
+      const rect = category.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        category.classList.add('visible');
+      }
+    });
+
+    const imageContainers = document.querySelectorAll('.image-container');
+    imageContainers.forEach(container => {
+      const rect = container.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        container.classList.add('visible');
+      }
+    });
+  }
 }
