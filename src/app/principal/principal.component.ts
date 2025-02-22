d8c3fddc4770259ac0f4dd35f997722d4eac8dcc
01jake <ang314256@gmail.com>
2025-02-22 01:03:00 -0700

    preview

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,8 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from "../footer/footer.component";
-import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-principal',
@@ -14,44 +12,45 @@ import { Router } from '@angular/router';
 export class PrincipalComponent {
   @ViewChild('miElemento') miElemento!: ElementRef; // Para acceder a un elemento del DOM
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
   ngAfterViewInit() {
-   
     this.miFuncionJavaScript();
   }
 
   miFuncionJavaScript() {
-    document.addEventListener('DOMContentLoaded', function() {
-      const carouselItems = document.querySelectorAll('.carousel-item');
-      let currentIndex = 0;
-  
-      function showNextSlide() {
-          carouselItems[currentIndex].classList.remove('active');
-          currentIndex = (currentIndex + 1) % carouselItems.length;
-          carouselItems[currentIndex].classList.add('active');
-      }
-  
-      setInterval(showNextSlide, 3000); // Cambia de imagen cada 5 segundos
-  });
-    // Aquí puedes escribir tu código JavaScript
-    document.addEventListener('DOMContentLoaded', function() {
-      const cards = document.querySelectorAll('.producto-card');
-  
-      const observer = new IntersectionObserver(entries => {
-          entries.forEach(entry => {
-              if (entry.isIntersecting) {
-                  entry.target.classList.add('visible');
-                  observer.unobserve(entry.target);
-              }
-          });
-      }, {
-          threshold: 0.1
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+
+    function showNextSlide() {
+      carouselItems[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      carouselItems[currentIndex].classList.add('active');
+    }
+
+    setInterval(showNextSlide, 3000); // Cambia de imagen cada 3 segundos
+
+    const cards = document.querySelectorAll('.producto-card');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
       });
-  
-      cards.forEach(card => {
-          observer.observe(card);
-      });
-  });
- 
-  
+    }, {
+      threshold: 0.1
+    });
+
+    cards.forEach(card => {
+      observer.observe(card);
+    });
   }
 }
